d50fed5775bac96c9587fac3c6b11c5aa99f749d
yidayo <418680861@qq.com>
2019-05-31 13:49:43 +0800

    1.1

--- rrtf-lesson/src/main/java/org/rrtf/lesson/App.java
+++ rrtf-lesson/src/main/java/org/rrtf/lesson/App.java
@@ -2,12 +2,17 @@ package org.rrtf.lesson;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App 
-{
+public class App extends SpringBootServletInitializer {
     public static void main( String[] args )
     {
         SpringApplication.run(App.class, args);
+    }
+    @Override//为了打包springboot项目
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
     }
 }
