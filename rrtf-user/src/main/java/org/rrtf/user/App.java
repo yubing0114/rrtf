46d893681c6455c8f7f55e0bad1e72935f4b53e1
yubing <2668113257@qq.com>
2019-06-02 10:24:04 +0800

    1.1.5

--- rrtf-user/src/main/java/org/rrtf/user/App.java
+++ rrtf-user/src/main/java/org/rrtf/user/App.java
@@ -1,13 +1,20 @@
 package org.rrtf.user;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class App extends SpringBootServletInitializer {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+	
+	@Override//为了打包springboot项目
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
     }
 }
