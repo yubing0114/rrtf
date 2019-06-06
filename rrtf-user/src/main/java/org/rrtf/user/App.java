18789b4faeb5dd04971e471021f56f3e9c9fdd48
yubing <418680861@qq.com>
2019-06-06 09:26:57 +0800

    1.1.7

--- rrtf-user/src/main/java/org/rrtf/user/App.java
+++ rrtf-user/src/main/java/org/rrtf/user/App.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class App extends SpringBootServletInitializer {
 	
 	public static void main(String[] args) {
